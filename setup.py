431b8d9be2c144dc58658f8b281d2a2167d6488d
Roman <roman.semein@gmail.com>
2025-04-11 18:19:20 -0400

    Bump version to 0.12.0 for PyPI release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import re
 
 # Version scheme based on existing PyPI versions
 # Last version on PyPI is 0.0.1.8, so we'll increment from there
-VERSION = '0.11.0'
+VERSION = '0.12.0'
 
 setup(
     name='voitta',
